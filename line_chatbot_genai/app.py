10f6d25a85a5d918f768a57dc1ea191eb57e8ed4
Rapin <rapin.pakdee@gmail.com>
2024-11-15 21:46:25 +0700

    add actual website

--- line_chatbot_genai/app.py
+++ line_chatbot_genai/app.py
@@ -693,20 +693,7 @@ def handle_message(event):
                 flex_list = gen_jreview_flex_product_list(pd_detail, answer_list)
                 flex_carousel = FlexCarousel(type='carousel',contents=flex_list)
                 messages_output = [FlexMessage(alt_text="jayreview", contents=flex_carousel)]
-        if 'iphone 16 pro max' in text.lower():
-            with open('/opt/config/jayhighlight.json', 'r', encoding='utf-8') as file:
-                json_string = file.read()
-            message_highlight = FlexMessage(alt_text="jayhighlight", contents=FlexContainer.from_json(json_string))
-            messages_output.append(message_highlight)
-            with ApiClient(configuration) as api_client:
-                line_bot_api = MessagingApi(api_client)
-                line_bot_api.reply_message(
-                    ReplyMessageRequest(
-                        reply_token=event.reply_token,
-                        messages=messages_output
-                    )
-                )
-        elif 'oppo a3 pro' in text.lower(): 
+        if 'oppo a3 pro' in text.lower(): 
             with open('/opt/config/jayhighlight_oppoa3pro.json', 'r', encoding='utf-8') as file:
                 json_string = file.read()
             message_highlight = FlexMessage(alt_text="jayhighlight", contents=FlexContainer.from_json(json_string))
@@ -719,8 +706,8 @@ def handle_message(event):
                         messages=messages_output
                     )
                 )
-        elif 'z fold6' in text.lower(): 
-            with open('/opt/config/jayhighlight_zfold6.json', 'r', encoding='utf-8') as file:
+        elif 's24 ultra' in text.lower(): 
+            with open('/opt/config/jayhighlight_s24ultra.json', 'r', encoding='utf-8') as file:
                 json_string = file.read()
             message_highlight = FlexMessage(alt_text="jayhighlight", contents=FlexContainer.from_json(json_string))
             messages_output.append(message_highlight)
